11c8c98ceac578a7ed3e25a74a3d2bb5fb1440ed
Mateo <mateog91@gmail.com>
2021-09-22 18:07:14 -0500

    ADD: file for pint function

---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: pointer to first element of stack
+ * @line_number: number of current line being executed
+ * 
+ * Description:
+ * prints the value at the top of the stack, followed by a new line.
+ * Usage: pint
+ * If the stack is empty, print the error message
+ * 
+ * Return: 0 on succes, exit with EXIT_FAILURE on failure
+ */
+int push(stack_t **stack, unsigned int line_number)
+{
+}
