8a6c8ebf02e4ea56a2782dfb1212a13b9cd12bf9
Mateo <mateog91@gmail.com>
2021-09-22 21:50:26 -0500

    style

--- pint.c
+++ pint.c
@@ -4,7 +4,7 @@
  * pint - the value at the top of the stack, followed by a new line.
  * @head: pointer to first node
  * @line_number: number of current line
- * 
+ *
  * Return: 0 on succes, -1 on failure
  */
 int pint(stack_t **head, unsigned int line_number)
